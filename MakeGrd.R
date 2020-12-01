96f08191a279bb4a23513ee778c9a4136d3e03e5
Kiri Daust <kiridaust@gmail.com>
2020-11-30 16:53:05 -0800

    Added files

---
+++ MakeGrd.R
@@ -0,0 +1,97 @@
+##read in provincial outline, tile, and output for climatebc
+##Kiri Daust
+
+library(data.table)
+library(sf)
+library(foreach)
+library(tidyverse)
+library(raster)
+library(rmapshaper)
+
+BC <- st_read(dsn = "./BigDat/BC_Province_Outline_Clean.gpkg")
+BC <- st_buffer(BC, dist = 0)
+BC <- ms_simplify(BC, keep = 0.2)
+grdAll <- st_make_grid(BC,cellsize = 40000, square = F, flat_topped = F)
+ptsAll <- st_centroid(grdAll)
+grdPts <- st_sf(ID = seq(length(ptsAll)), geometry = ptsAll)
+st_write(grdPts, dsn = "/media/kiridaust/MrBig/BCGrid/HexPts400.gpkg", layer = "HexPts400", driver = "GPKG", overwrite = T, append = F)
+  grdPoly <- st_sf(ID = seq(length(grdAll)),geometry = grdAll)
+st_write(grdPoly, dsn = "/media/kiridaust/MrBig/BCGrid/HexGrd400", layer = "HexGrd400", driver = "GPKG")
+
+vertDist <- function(x){(sideLen(x)*3)/2}
+sideLen <- function(x){x/sqrt(3)}
+
+tiles <- st_make_grid(BC, cellsize = c(305000,vertDist(305000)))
+plot(tiles)
+plot(BC, add = T)
+
+bgc <- st_read(dsn = "~/CommonTables/WNA_BGC_v12_12Oct2020.gpkg")
+bgc <- bgc[is.na(bgc$State),c("BGC")]
+
+dem <- raster("./BigDat/BC_25m_DEM_WGS84.tif")
+
+datOut <- foreach(tile = 1:length(tiles), .combine = rbind) %do% {
+  cat("Processing tile",tile,"... \n")
+  testTile <- tiles[tile,]
+  testGrd <- st_intersection(grdPts, testTile)
+  grdBGC <- st_join(testGrd,bgc)
+  grdBGC$el <- raster::extract(dem, grdBGC)
+  grdBGC <- st_transform(grdBGC, 4326)
+  out <- cbind(st_drop_geometry(grdBGC),st_coordinates(grdBGC)) %>% as.data.table()
+  out <- out[,.(ID1 = ID, ID2 = MAP_LABEL, lat = Y, long = X, el)]
+  out[,TileNum := tile]
+  out
+}
+
+dat <- unique(datOut, by = "ID1")
+tileID <- dat[,.(ID1,TileNum)]
+
+for(i in 1:length(unique(tileID$TileNum))){
+  dat2 <- dat[TileNum == i,]
+  dat2[,TileNum := NULL]
+  dat2 <- dat2[complete.cases(dat2),]
+  fwrite(dat2, paste0("Tile",i,"_In.csv"), eol = "\r\n")
+}
+
+
+
+####old code
+temp <- st_sf(tiles, ID = seq(65))
+t1 <- temp[1,]
+t2 <- st_intersection(t1,BC)
+
+tile_width <- st_bbox(tiles[2,])[1] - st_bbox(tiles[1,])[1]
+
+grd1 <- st_make_grid(tiles[1,],cellsize = 400, square = F, flat_topped = F)
+grd2 <- st_make_grid(tiles[2,],cellsize = 400, square = F, flat_topped = F)
+grd3 <- st_make_grid(tiles[8,],cellsize = 400, square = F, flat_topped = F)
+
+plot(tiles[c(1,2,8),])
+plot(grd1, add = T)
+plot(grd2, add = T)
+plot(grd3, add = T)
+
+test <- st_difference(grd1,st_union(grd2))
+plot(tiles[c(1,2,8),])
+plot(test, add = T)
+
+grd1 <- st_make_grid(tiles[1,],cellsize = 2000,square = F, flat_topped = T)
+grd2 <- st_make_grid(tiles[2,],cellsize = 2000,square = F, flat_topped = T)
+grd3 <- st_make_grid(tiles[3,],cellsize = 2000,square = F, flat_topped = T)
+
+out <- st_sf(geom = c(grd1,grd2,grd3),ID = rep(c(1,2,3), c(length(grd1),length(grd2),length(grd3))))
+st_write(out, dsn = "TestGrid",layer = "Test4", driver = "ESRI Shapefile",append = T, overwrite = T)
+
+temp <- st_sf(geom = grd2, id = seq(length(grd2)))
+plot(temp[1,])
+st_area(temp[1,])
+
+#grd2 <- st_make_grid(t2,offset = c(bb[1],bb[2]), cellsize = 1000, square = F)
+t3 <- c(grd1,grd2)
+plot(t3)
+st_write(t3, dsn = "TestGrid",layer = "test1", driver = "ESRI Shapefile",append = F, overwrite = T)
+st_write(tiles[1:2,],dsn = "TestGrid",layer = "Tiles", driver = "ESRI Shapefile", append = T)
+st_write(grd4, dsn = "TestGrid",layer = "Offset2", driver = "ESRI Shapefile",append = T)
+
+grdAll <- st_make_grid(BC, cellsize = 1000, square = F)
+
---
+++ RunPredictHex.R
@@ -0,0 +1,114 @@
+require(data.table)
+require(doBy)
+require(randomForest)
+require(foreach)
+require(dplyr)
+require(reshape2)
+library(doParallel)
+library(tidyr)
+require(ranger)
+require(DBI)
+require(sf)
+library(here)
+require(RPostgreSQL)
+
+addVars <- function(dat){
+  dat[,`:=`(PPT_MJ = PPT05+PPT06,
+            PPT_JAS = PPT07+PPT08+PPT09,
+            PPT.dormant = PPT_at+PPT_wt)]
+  dat[,`:=`(CMD.def = 500-PPT.dormant)]
+  dat[CMD.def < 0, CMD.def := 0]
+  dat[,`:=`(CMDMax = CMD07,
+            CMD.total = CMD.def +CMD)]
+  return(dat)
+}
+
+drv <- dbDriver("PostgreSQL")
+con <- dbConnect(drv, user = "postgres", host = "localhost",password = "Kiriliny41", port = 5432, dbname = "cciss_data")
+
+datDir <- "~/ClimBC_Tiles/"
+load("WNAv11_35_VAR_SubZone_ranger.Rdata")
+varImport <- c("Year","ID1","ID2", "Tmax_sp", "Tmax_sm", "Tmin_wt", "Tmin_sp", "Tave_at", "PPT_wt", 
+  "PPT_sp", "PPT_sm", "PPT_at", "MSP",
+  "DD5_wt", "DD5_sp", "DD5_sm", "DD5_at", "PAS_wt", "PAS_sp", "PAS_sm", 
+  "PAS_at", "Eref_sp", "Eref_sm", "NFFD", "bFFP", "eFFP", "MWMT", 
+  "MCMT", "AHM", "SHM", "CMD", "PPT05", "PPT06", "PPT07", "PPT08", 
+  "PPT09", "CMD07","CMD_sp", "CMD_at")
+
+tile_predict <- function(Y1, maxSize = 6000000){
+  n = nrow(Y1)
+  brks <- seq(1,n,by = maxSize)
+  brks <- c(brks,n)
+  Y1[,BGC.pred := NA_character_]
+  for(j in 1:(length(brks)-1)){
+    Y1[brks[j]:brks[j+1],BGC.pred := predict(BGCmodel, Y1[brks[j]:brks[j+1],-c(1:3)])[['predictions']]]
+  }
+  TRUE
+}
+
+
+for(i in 14:19){
+  cat("Processing tile",i,"... \n")
+    dat <- fread(paste0(datDir,"Tile",i,"_Out.csv"),select = varImport)
+    Y1 <- addVars(dat)
+    
+    vars <- BGCmodel[["forest"]][["independent.variable.names"]]
+    varList = c("Model", "SiteNo", "BGC", vars)
+    colnames (Y1) [1:3] = c("Model", "SiteNo", "BGC")
+    Y1=Y1[,..varList]
+    Y1 <- Y1[complete.cases(Y1),]
+    
+    ##Predict future subzones######
+    tile_predict(Y1)
+    gc()
+    Y1 <- separate(Y1, Model, into = c("GCM","Scenario","FuturePeriod"), sep = "_", remove = T)
+    gc()
+    Y1$FuturePeriod <- gsub(".gcm","",Y1$FuturePeriod)
+    Y1 <- Y1[,c("GCM","Scenario","FuturePeriod","SiteNo","BGC","BGC.pred")]
+    setnames(Y1, c("gcm","scenario","futureperiod","siteno","bgc","bgc_pred"))
+    dbWriteTable(con, "cciss_400m", Y1,row.names = F, append = T)
+    rm(Y1,dat)
+    gc()
+    
+}
+
+grd <- st_read(dsn = "../BCGrid/HexGrd400.gpkg")
+pts <-st_read(dsn = "../BCGrid/HexPts400.gpkg")
+colnames(grd)[1] <- "id"
+st_write(grd, con, drop = T)
+test <- st_read(con,query = "SELECT * FROM grd WHERE id IN (5,6);")
+
+districts <- st_read(dsn = "../CommonTables/ForestRegions.gpkg", layer = "ForestRegions_clipped")
+districts <- districts[,c("REGION_NAM","ORG_UNIT")]
+
+distJn <- st_join(pts,districts)
+distJn <- distJn[!is.na(distJn$ORG_UNIT),]
+dj <- st_drop_geometry(distJn) %>% as.data.table()
+dj <- dj[!is.na(ORG_UNIT),]
+
+districts <- st_read(dsn = "../CommonTables/ForestRegions.gpkg", layer = "ForestDistricts")
+districts <- districts[,c("DISTRICT_N","ORG_UNIT")]
+distJn2 <- st_join(pts,districts)
+dj2 <- st_drop_geometry(distJn2) %>% as.data.table()
+dj2 <- dj2[!is.na(ORG_UNIT),]
+
+tID <- fread(file.choose())
+
+dj3 <- dj2[dj, on = "ID"]
+setnames(tID, c("ID","TileNum"))
+dj3 <- tID[dj3, on = "ID"]
+setnames(dj3,c("siteno","tileno","district","dist_code","region","reg_code"))
+
+dbWriteTable(con,"id_atts", dj3,row.names = F)
+dbGetQuery(con, "select distinct region from id_atts")
+
+  sk <- districts[4,]
+
+sites <- dbGetQuery(con, "select distinct siteno from cciss_400m")
+sites <- dbGetQuery(con, "select * from cciss_400m 
+                    inner join id_atts on cciss_400m.siteno = id_atts.siteno 
+                    where reg_code = 'RSK'")
+sno <- dbGetQuery(con, "select siteno from id_atts where reg_code = 'RSK'")
+
+q <- paste0("select * from cciss_400m where siteno in (",paste(sno$siteno, collapse = ","),")")      
+test <- dbGetQuery(con,q)
