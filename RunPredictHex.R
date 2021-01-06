4e36a295642783ceb646954bc631abb3701fb643
Kiri Daust <kiridaust@gmail.com>
2021-01-06 12:44:52 -0800

    Minor update

--- RunPredictHex.R
+++ RunPredictHex.R
@@ -13,6 +13,65 @@ require(sf)
 library(here)
 require(RPostgreSQL)
 
+##this part for updating ID on climateBC outputs
+# IDCross <- fread("Old_NewHexCrosswalk.csv")
+# varNames <- fread(paste0("~/ClimBC_Tiles/Tile",1,"_Out.csv"), nrows = 0)
+# varNames <- colnames(varNames)
+# varNames <- c(varNames[c(1:6,175:253)],"PPT05", "PPT06", "PPT07", "PPT08", 
+#               "PPT09","Tmax07","DD5_05","DD5_06","DD5_07","DD5_08","DD5_09",
+#               "CMD05","CMD06","CMD07","CMD08","CMD09","Tave10","Tave04")
+# deleteDT <- function(DT, del.idxs) {           # pls note 'del.idxs' vs. 'keep.idxs'
+#   keep.idxs <- setdiff(DT[, .I], del.idxs);  # select row indexes to keep
+#   cols = names(DT);
+#   DT.subset <- data.table(DT[[1]][keep.idxs]); # this is the subsetted table
+#   setnames(DT.subset, cols[1]);
+#   for (col in cols[2:length(cols)]) {
+#     DT.subset[, (col) := DT[[col]][keep.idxs]];
+#     DT[, (col) := NULL];  # delete
+#   }
+#   return(DT.subset);
+# }
+# 
+# dat <- fread("~/ClimBC_Tiles/NormalData/Tile6_In_Normal_1961_1990MSY.csv", select = "ID1")
+# dat2 <- fread("~/ClimBC_Tiles/Tile14_Out.csv", select = "ID1")
+# dat2 <- unique(dat2)
+# 
+# for(i in c(2:13)){
+#   dat <- fread(paste0("~/ClimBC_Tiles/NormalData/Tile",i,"_In_Normal_1961_1990MSY.csv"))
+#   dat[IDCross, NewID := i.NewID, on = c(ID1 = "OldID")]
+#   dat[,ID1 := NULL]
+#   nms <- colnames(dat)
+#   nms <- nms[c(1,length(nms),2:(length(nms)-1))]
+#   setcolorder(dat, nms)
+#   toDelete <- which(is.na(dat$NewID))
+#   dat <- deleteDT(dat,toDelete)
+#   gc()
+#   fwrite(dat,file = paste0("~/Desktop/BCHex_ClimateBC/Normal/Tile",i,"_Norm.csv"))
+#   rm(dat)
+#   gc()
+# }
+# 
+# decs <- c("1991_2000","2001_2010","2011_2019")
+# for(i in c(2:13)){
+#   dat <- foreach(j = decs, .combine = rbind) %do% {
+#     temp <- fread(paste0("~/ClimBC_Tiles/CurrentData/Tile",i,"_In_Decade_",j,"MSY.csv"))
+#     #temp <- fread(paste0("~/ClimBC_Tiles/CurrentData/NewPnts_In_Decade_",j,"MSY.csv"))
+#   }
+#   dat <- dat[,lapply(.SD, mean), by = .(ID1,ID2)]
+#   dat[IDCross, NewID := i.NewID, on = c(ID1 = "OldID")]
+#   dat[,ID1 := NULL]
+#   nms <- colnames(dat)
+#   nms <- nms[c(1,length(nms),2:(length(nms)-1))]
+#   setcolorder(dat, nms)
+#   toDelete <- which(is.na(dat$NewID))
+#   dat <- deleteDT(dat,toDelete)
+#   gc()
+#   fwrite(dat,file = paste0("~/Desktop/BCHex_ClimateBC/Current/Tile",i,"_Curr.csv"))
+#   rm(dat)
+#   gc()
+# }
+
+
 addVars <- function(dat){
   dat[,`:=`(PPT_MJ = PPT05+PPT06,
             PPT_JAS = PPT07+PPT08+PPT09,
@@ -26,6 +85,19 @@ addVars <- function(dat){
 
 drv <- dbDriver("PostgreSQL")
 con <- dbConnect(drv, user = "postgres", host = "localhost",password = "Kiriliny41", port = 5432, dbname = "cciss_data")
+
+grd <- st_read("BC_HexPoly400m.gpkg")
+st_write(grd, dsn = con,"hex_grid")
+districts <- st_read(file.choose())
+districts <- districts[,c("DISTRICT_N","ORG_UNIT","REGION_ORG")]
+pnts <- st_read("BC_HexPoints400m.gpkg")
+
+grd2 <- st_join(pnts,districts)
+colnames(grd2)[2:4] <- c("district","dist_code","reg_code")
+st_write(grd2, dsn = con,"hex_points")
+atts <- as.data.table(st_drop_geometry(grd2))
+atts <- atts[!is.na(dist_code),]
+dbWriteTable(con,"id_atts",atts, row.names = F)
 
 # crosstab <- fread("Old_NewHexCrosswalk.csv")
 # crosstab <- crosstab[!is.na(OldID),]
