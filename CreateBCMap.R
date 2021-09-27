c7145c8cfefcccd260371a6976ddc557893891a9
Will MacKenzie <34073318+whmacken@users.noreply.github.com>
2021-09-27 15:37:28 -0700

    fixed postgres source

--- CreateBCMap.R
+++ CreateBCMap.R
@@ -20,8 +20,11 @@ con <- dbConnect(drv, user = "postgres",
 #con <- dbConnect(drv, user = "postgres", host = "smithersresearch.ca",password = "Kiriliny41", port = 5432, dbname = "cciss_data") ### for external use
 
 ###read grid
+# tic()
+# hexGrid <- st_read(con, query = "select * from hex_grid")
+# toc()
 tic()
-hexGrid <- st_read(con, query = "select * from hex_grid")
+hexGrid <- st_read("F:/OneDrive - Government of BC/CCISSv12/working_files/HexGrid400m_Sept2021.gpkg")
 toc()
 #st_write(hexGrid,"./outputs/basehex.gpkg", append = FALSE)
 
@@ -35,32 +38,51 @@ toc()
 
 ##just so you know what options are available
 gcms <- dbGetQuery(con,"select distinct gcm from future_params")[,1]
+gcms
 futureperiods <- dbGetQuery(con,"select distinct futureperiod from future_params")[,1]
 rcps <- dbGetQuery(con,"select distinct scenario from future_params")[,1]
 
 ### select options
-gcm <- "EC-Earth3"
+gcm <- "ACCESS-ESM1-5"
 futureperiod <- "2041-2060"
 rcp <- "ssp245"
 
-q <- paste0("select siteno,bgc_pred from cciss_future12 where gcm = '",gcm,"' and futureperiod = '", futureperiods,"' and scenario = '",rcp,"'")
+q <- paste0("select siteno,bgc_pred from cciss_future12 where gcm = '",gcm,"' and futureperiod = '", futureperiod,"' and scenario = '",rcp,"'")
 tic()
 datPred <- setDT(dbGetQuery(con,q))## read from database
 toc()
-
- colnames(hexGrid)[1] <- "siteno"
- tic()
- hexGrid <- as.data.table(hexGrid)
- toc()
- ##convert to data table because join is much faster
+#datPred2 <- as.data.table(datPred) %>% distinct()
+ #colnames(hexGrid)[1] <- "siteno"
+ #fwrite(datPred, "./outputs/datPred.csv")
+#datPred.missing <- datPred %>% filter(is.na(bgc_pred))  
+# tic()
+ hexGrid <- as.data.table(hexGrid) #%>% distinct()##convert to data table because join is much faster
+# toc()
+# hexGrid2 <- hexGrid1 %>% select(siteno, bgc_pred)
+# fwrite(hexGrid2, "./outputs/hexGrid.csv")
+#hexGrid <- hexGrid1
+#hexGrid2 <- left_join(hexGrid,datPred)
+#hexGrid3 <- hexGrid2 %>% distinct()
  hexGrid[datPred, bgc_pred := i.bgc_pred, on = "siteno"]
+ hexGrid <- hexGrid %>% select(-BGC)
 hexGrid <- hexGrid %>%  rename(BGC = bgc_pred)
- 
+#hexGrid.missing <- hexGrid %>% filter(is.na(BGC)) 
+#misjoint <- anti_join(hexGrid, datPred, by = "siteno")
+#misjoint2 <- anti_join(datPred, hexGrid, by = "siteno")
+#misjoint <- misjoint %>% filter(is.na(BGC))
  hexGrid.pred <-  st_as_sf(hexGrid)
- 
-st_write(hexGrid.pred,"./outputs/EC-Earth3_r45_2050.gpkg", append = FALSE)
+hexGrid.missing <- hexGrid.pred %>% filter(is.na(BGC)) 
+hexGrid.have <- hexGrid.pred %>% filter(!is.na(BGC)) 
+hexmiss <- hexGrid.missing$siteno
+
+st_write(hexGrid.pred,"./outputs/ACCESS-ESM1_r45_2050.gpkg", append = FALSE, delete_dsn = TRUE)
 
 
+hexGrid2 <- as.data.table(hexGrid)
+hexGrid2 <- hexGrid2[hexGrid2$siteno %in% hexmiss, ]
+datPred2 <- datPred[datPred$siteno %in% hexmiss,]
+
+st_write(hexGrid.missing,"./outputs/NoBGChexGrid.gpkg", append = FALSE)
 
 ### this is now old stuff, but some could be reused.
 ## pull district codes from database
